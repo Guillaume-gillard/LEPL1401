49ffae003c90de5cf10281e8e2569d866fffb78f
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:17 +0100

    Create maximum.py

---
+++ Session 3/maximum.py
@@ -0,0 +1,15 @@
+# Guillaume Gillard
+# 07/10/2022
+
+### Code to complete [START] ###
+
+def afficheMax(a, b):
+    """
+    pre: None
+    post: affiche le maximum entre les nombres entiers `a` et `b`
+    """
+    if a > b :
+        max = a
+    else :
+        max = b
+    print(max)
