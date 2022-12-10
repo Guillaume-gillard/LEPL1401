04670f0551752c7566d9e0eacae1221dc65068ca
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 20:54:59 +0100

    Create anonymous-informations-extraction.py

---
+++ Session 4/anonymous-informations-extraction.py
@@ -0,0 +1,29 @@
+# Guillaume Gillard
+# 21/10/2022
+
+
+### Code to complete [START] ###
+
+import string 
+    
+def extract(code):
+    code = str(code)
+    composition = ""
+    alphabet = string.ascii_lowercase + string.ascii_uppercase # voir docu python
+    number = "1234567890"
+    vowel = "aeiouy"
+    vowel_up = vowel.upper()
+    consonant_upper = "B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z"
+    consonant = consonant_upper.lower()
+    for i in code:
+        if i in vowel_up :
+            composition += "vowel-up "
+        if i in vowel :
+            composition += "vowel-low "
+        if i in consonant :
+            composition += "consonant-low "
+        if i in consonant_upper :
+            composition += "consonant-up "
+        elif i in number :
+            composition += "number "
+    return composition
