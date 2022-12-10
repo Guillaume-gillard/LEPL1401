5ec21fe3453e0df5fa8fcf8dc4a64b987a5d7cd5
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 19:14:49 +0100

    Create amende.py

---
+++ Session 3/amende.py
@@ -0,0 +1,19 @@
+# Guillaume Gillard
+# 08/10/2022
+
+### Code to complete [START] ###
+
+
+def fine(authorized_speed, actual_speed):
+    exces = actual_speed - authorized_speed
+    fine_amount = 0
+    for i in range(1, exces+1):
+        fine_amount += 5
+    if exces <= 0:
+        return(0)
+    elif fine_amount < 12.5 :
+        return(12.5)
+    elif exces <= 10 and fine_amount > 12.5 :
+        return(fine_amount)
+    else :
+        return(fine_amount * 2)
