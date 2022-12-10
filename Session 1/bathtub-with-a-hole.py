16fe66893967a14bc4becdb6daffc3225cfeb61f
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:13 +0100

    Create bathtub-with-a-hole.py

---
+++ Session 1/bathtub-with-a-hole.py
@@ -0,0 +1,12 @@
+# Guillaume Gillard
+# 15/10/2022
+
+### Code to complete [START] ###
+
+water_vol = 0
+filled_time  = 0
+
+while water_vol < 80:
+    water_vol += 11
+    filled_time += 1
+filled_time = filled_time + ((80 - water_vol)/11)
