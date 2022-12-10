74445b97b2c37c875d796f6d47132712a15ea5bb
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:24 +0100

    Create somme-a-completer.py

---
+++ Session 2/somme-a-completer.py
@@ -0,0 +1,16 @@
+# Guillaume Gillard
+# 05/10/2022
+
+some_value = ...
+
+### code to complete [START] ###
+
+n = some_value
+sum = 0
+count = 0
+for numb in range(1, 20000):
+    if count == n :
+        break
+    elif numb % 2 == 0:
+        sum += numb
+        count += 1
