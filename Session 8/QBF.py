5ff084300bcaba39f229e95315ab748c8cd53f0d
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:22:12 +0100

    Create QBF.py

---
+++ Session 8/QBF.py
@@ -0,0 +1,20 @@
+# Guillaume Gillard 
+# 25/11/2022 
+
+### Code to complete [START] ###
+
+class Employe :
+    def __init__(self, n, s):
+        self.nom = n
+        self.salaire = s    
+   
+
+    def __str__(self):
+        info = "{} : {}".format(self.nom, self.salaire)
+        return info
+    
+    
+    def augmente(self, augm):
+        pourcent = 1 + (augm)/100
+        new_salary = self.salaire * pourcent
+        self.salaire = new_salary
