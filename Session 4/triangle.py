30f7195aa6d43cb29e4ab899fb37f75ee04032ad
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 19:58:29 +0100

    Create triangle.py

---
+++ Session 4/triangle.py
@@ -0,0 +1,14 @@
+# Guillaume Gillard
+# 19/10/2022
+
+
+### Code to complete [START] ###
+
+def triangle(n):
+    l = []
+    for i in range(n+1):
+        s = []
+        for x in range(i+1):
+            s.append(x)
+        l.append(s)
+    return(l)
