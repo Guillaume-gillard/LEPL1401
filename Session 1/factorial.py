5c1f1e573f3a905c92c3387f34390fcb2be42845
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:32 +0100

    Create factorial.py

---
+++ Session 1/factorial.py
@@ -0,0 +1,12 @@
+# Guillaume Gillard
+# 05/10/2022
+
+x = ... #the number
+result = ... #store in this variable the factorial of x
+
+### Code to complete [START] ###
+
+result = 1
+while x > 0:
+    result = x * result 
+    x -= 1
