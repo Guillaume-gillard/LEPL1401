4be9d30d6d47492406e722e1450e9e748d90154f
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 19:58:25 +0100

    Create prime-numbers.py

---
+++ Session 4/prime-numbers.py
@@ -0,0 +1,18 @@
+# Guillaume Gillard
+# 09/11/2022
+
+### Code to complete [START] ###
+
+def is_prime(i, prime_number):
+    for x in prime_number:
+        if i%x == 0:
+            return False
+    return True
+
+
+def primes(max):
+    prime_number = []
+    for i in range(2, max+1):
+        if is_prime(i, prime_number):
+            prime_number.append(i)
+    return prime_number
