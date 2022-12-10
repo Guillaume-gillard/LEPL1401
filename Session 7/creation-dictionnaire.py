fffd8e5ebb086791b4c765fb4c900ff59edb0bed
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:22:20 +0100

    Create creation-dictionnaire.py

---
+++ Session 7/creation-dictionnaire.py
@@ -0,0 +1,17 @@
+# Guillaume Gillard 
+# 15/11/2022
+
+### Code to complete [START] ###
+
+def create_dict(l):
+    d = {}
+    for x, y in l :
+        found = False
+        for key in d:
+            if x == key :
+                found = True
+        if found :
+            d[x] += [y]
+        else :
+            d[x] = [y]
+    return d
