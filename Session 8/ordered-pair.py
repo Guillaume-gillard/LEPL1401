ebeca0e6b696088dd6811f266fdca037bb1736a1
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:22:16 +0100

    Create ordered-pair.py

---
+++ Session 8/ordered-pair.py
@@ -0,0 +1,37 @@
+# Guillaume Gillard
+# 22/11/2022
+
+class Pair :
+    pass 
+
+class OrderedPair:
+    '''
+    Représente une paire de deux entiers (représenté en interne par une instance p de la classe Pair).
+    Cette paire est considérée comme ordonnée si l'attribut a de p est plus petit ou égal à son attribut b
+    '''
+
+    def __init__(self):
+        self.p = Pair(0, 0)
+        self.ordered = True
+
+
+### Code to complete [START] ###
+
+
+    def set_a(self, n_a):
+        """
+        @pre: None
+        @post: donne au premier élément de la paire la nouvelle valeur n_a
+        """
+        self.p.a = n_a
+        self.ordered = self.p.a<=self.p.b
+
+
+    def set_b(self, n_b):
+        """
+        @pre: None
+        @post: donne au second élément de la paire la nouvelle valeur n_b
+        """
+        self.p.b = n_b
+        self.ordered = self.p.a<=self.p.b
+        
