5bc66e6586daadeefbf9e083a77018704fb86395
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:21:46 +0100

    Create tri-de-liste.py

---
+++ Session 5/tri-de-liste.py
@@ -0,0 +1,24 @@
+# Guillaume Gillard 
+# 08/11/2022
+
+a_list = ... #list to sort
+sorted_list = ... #store in this variable the sorted version of the list
+
+### Code to complete [START] ###
+
+def sorting(a_list):
+    tri = [a_list[0]]
+    for x in a_list[1:]:
+        if x < tri[0]:
+            tri = [x] + tri
+        elif x >= tri[len(tri)-1]:
+            tri += [x]
+        else :
+            for place in range(len(tri)):
+                if tri[place] <= x and x <= tri[place+1]:
+                    tri = tri[:place+1] + [x] + tri[place+1:]
+                    break
+    return tri
+              
+sorted_list = sorting(a_list)
+            
