fbddee6bed7a8587690ddf1ad3733e95a6686e5d
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:22:10 +0100

    Create student-init.py

---
+++ Session 8/student-init.py
@@ -0,0 +1,11 @@
+# Guillaume Gillard
+# 20/11/2022
+
+### Code to complete [START] ###
+
+class Student:
+    def __init__(self, firstname , surname , birthday , email):
+        self.firstname = firstname
+        self.surname = surname
+        self.birthday = birthday
+        self.email = email
