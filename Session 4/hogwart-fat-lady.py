3a14474acd70862b986f8b2f28a7800045945510
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 20:54:47 +0100

    Create hogwart-fat-lady.py

---
+++ Session 4/hogwart-fat-lady.py
@@ -0,0 +1,10 @@
+# Guillaume Gillard
+# 19/10/2022
+
+
+### Code to complete [START] ###
+
+def portrait(right_password, entered_password):
+    if right_password == entered_password:
+        return(True)
+    return(False)
