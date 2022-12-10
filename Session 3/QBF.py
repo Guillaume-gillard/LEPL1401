329a314651f20f1150b421c16b5ee2402be63c18
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:57:32 +0100

    Create QBF.py

---
+++ Session 3/QBF.py
@@ -0,0 +1,14 @@
+# Guillaume Gillard
+# 08/10/2022
+
+### Code to complete [START] ###
+
+def chiffres_pairs(n):
+    count = 0
+    while n != 0 :
+        n = n // 10
+        count += 1
+    if count % 2 == 0 and count != 0: #si n = 0 => count = 0 et 0%2 = 0 alors que 0 a un nbr impair de chiffres
+        return(True)
+    else :
+        return False
