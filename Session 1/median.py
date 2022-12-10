3291be0a22774c309da1a4feca961abde5b056c7
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:35 +0100

    Create median.py

---
+++ Session 1/median.py
@@ -0,0 +1,16 @@
+# Guillaume Gillard
+# 06/10/2022
+
+a = ... #variable to evaluate
+b = ... #variable to evaluate
+c = ... #variable to evaluate
+median = ... #store in this variable the median of the three variables
+
+### Code to complete [START] ###
+
+if a < b < c or c < b < a :
+    median = b
+elif c < a < b or b < a < c :
+    median = a
+elif a < c < b or b < c < a :
+    median = c
