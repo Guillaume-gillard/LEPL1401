f538f1dd61db049923e5d1fb9608abd2f5369fde
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:50 +0100

    Create fizzbuzz.py

---
+++ Session 2/fizzbuzz.py
@@ -0,0 +1,16 @@
+# Guillaume Gillard 
+# 06/10/2022
+
+i = ...    # the number to check (i >= 1)
+temp = ... # "fizz", "buzz", "fizzbuzz" or "no" depending on i's value
+
+### Code to complete [START] ###
+
+if i % 3 == 0 and i % 5 == 0:
+    temp = "fizzbuzz"
+elif i % 3 == 0:
+    temp = "fizz"
+elif i % 5 == 0:
+    temp = "buzz"
+else :
+    temp = "no"
