287756d0c91e4dbf7f5f181834cce4aca376082c
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:07:11 +0100

    Create nombres-premiers.py

---
+++ Session 2/nombres-premiers.py
@@ -0,0 +1,12 @@
+# Guillaume Gillard 
+# 07/10/2022
+
+is_prime = True
+n = ...             #Un entier supérieur à 1
+
+### code to complete [START] ###
+
+for div in range(2, n):
+    if div != n and n % div == 0:
+        is_prime = False 
+        break # car si il existe un div qui n'est pas n, n d'office pas premier 
