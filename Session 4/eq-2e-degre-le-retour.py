0f123b94ba18210b32b2272a2ddbe0a46cfdb739
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 20:54:51 +0100

    Create eq-2e-degre-le-retour.py

---
+++ Session 4/eq-2e-degre-le-retour.py
@@ -0,0 +1,25 @@
+# Guillaume Gillard
+# 09/11/2022
+
+def solution(a, b, c):
+    """
+    pre:  a, b et c sont 3 nombres réels
+    post: la valeur de retour de cette fonction dépend du nombre
+          de solutions de l'équation ax^2 + bx + c = 0.
+    - 0 solution: retourne la liste vide
+    - 1 solution: retourne une liste contenant la solution de l'équation
+    - 2 solutions: retourne une liste contenant les deux solutions,
+                  la plus petite solution en première place
+    """
+    pass
+
+### Code to complete [START] ###
+
+def solveur(eq_list):
+    sol = []
+    for eq in eq_list:
+        a = eq[0]
+        b = eq[1]
+        c = eq[2]
+        sol.append(solution(a, b, c))
+    return sol
