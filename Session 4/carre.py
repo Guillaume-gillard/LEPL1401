4cd4a240bd73521a5e19a1dca8f5ba60dec34add
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 19:59:08 +0100

    Create carre.py

---
+++ Session 4/carre.py
@@ -0,0 +1,17 @@
+# Guillaume Gillard
+# 21/10/2022 
+
+
+### Code to complete [START] ###
+
+
+def carre(n):
+    matrix = []
+    count = 0
+    for l in range(n):
+        row = []
+        for i in range(n):
+            row.append((count)*n + i)
+        matrix.append(row)
+        count += 1
+    return matrix
