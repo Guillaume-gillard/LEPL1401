75365695023b2b0302ec48d4cabcf46dcb8faf93
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:06:54 +0100

    Create interval.py

---
+++ Session 2/interval.py
@@ -0,0 +1,15 @@
+# Guillaume Gillard
+# 06/10/2022
+
+a = ... #lower bound of the interval
+b = ... #upper bound of the interval
+x = ... #variable to evaluate
+interval = ... #store in this variable the belonging of x in the interval
+
+### Code to complete [START] ###
+
+if x >= a and x <= b :
+    interval = True 
+else :
+    interval = False
+
