f55e8d26a135664f134ab3aacda78125052a0a5c
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:07:18 +0100

    Create QBF.py

---
+++ Session 2/QBF.py
@@ -0,0 +1,14 @@
+# Guillaume Gillard
+# 06/10/2022
+
+n = ...    #Borne supérieure
+
+### code to complete [START] ###
+
+for i in range(1, n+1):
+    count = 0
+    for div in range(1, i+1):
+        if div != i and i % div == 0:
+            count += 1
+    print(i, ":", count)
+    
