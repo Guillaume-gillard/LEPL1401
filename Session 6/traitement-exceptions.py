2633d0f224b8ebb82c3aace166f3474d1aa979f1
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:22:01 +0100

    Create traitement-exceptions.py

---
+++ Session 6/traitement-exceptions.py
@@ -0,0 +1,27 @@
+# Guillaume Gillard
+# 04/11/2022
+
+### Code to complete [START] ###
+
+try :
+    command = input ( "Enter your command: " )
+
+except :
+    print("There was an error")
+
+parameters = command.split ()
+
+if parameters[0] == "divide":
+    try :
+        print ( "The value of your division is: " + str (float(parameters[1])/float(parameters[2])))
+    except :
+        print("There was an error")
+elif parameters[0] == "showfile":
+    try :
+        file = open ( parameters[1] )
+        print ( file.read () )
+        file.close ()
+    except :
+        print("There was an error")
+else:
+    print ( "Command not recognized")
