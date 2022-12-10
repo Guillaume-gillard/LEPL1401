c1c1224b009d2e5c925d9f7ea249b508ade6c446
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 20:54:53 +0100

    Create difference-counter.py

---
+++ Session 4/difference-counter.py
@@ -0,0 +1,17 @@
+# Guillaume Gillard
+# 02/11/2022
+
+def diff_count(lst):
+
+    ### Code to complete [START] ###
+
+    allreadycounted = []
+    count = 0
+    item_2 = ""
+    for item in lst :
+        item_1 = item
+        if item_1 != item_2 and item_1 not in allreadycounted:
+            allreadycounted.append(item)
+            count +=1
+        item_2 = item_1
+    return count
