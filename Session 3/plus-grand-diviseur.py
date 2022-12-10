05ccb721a62856f2ebb547df0e60ca70ad97ffbe
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 18:57:29 +0100

    Create plus-grand-diviseur.py

---
+++ Session 3/plus-grand-diviseur.py
@@ -0,0 +1,17 @@
+# Guillaume Gillard
+# 14/10/2022
+
+
+def greatest_divisor(a):
+
+    ### Code to complete [START] ###
+
+    div_1 = 1
+    div_2 = 1
+
+    for div in range(2, a+1):
+        div_2 = div_1
+        if a % div == 0:
+            div_1 = div
+        if a == div:
+            return(div_2)
