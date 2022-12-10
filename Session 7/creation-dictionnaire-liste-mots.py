f46c2e96aa4b63b611fc22cc587cf49a40d99a77
Guillaume <gillard.guillaume@gmail.com>
2022-12-10 21:22:05 +0100

    Create creation-dictionnaire-liste-mots.py

---
+++ Session 7/creation-dictionnaire-liste-mots.py
@@ -0,0 +1,16 @@
+# Guillaume Gillard
+# 11/11/2022
+
+### Code to complete [START] ###
+
+
+def create_index(list_of_words):
+    d = {}
+    pos = 0
+    for words in list_of_words :
+        if words not in d :
+            d[words] = [pos]
+        else :
+            d[words] += [pos]
+        pos += 1
+    return d
