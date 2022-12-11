df5e60d432d66c445b6858359dba89a5cf3cbb9f
Guillaume <gillard.guillaume@gmail.com>
2022-12-11 18:15:03 +0100

    Create linkedlist-init.py

---
+++ Session 11/linkedlist-init.py
@@ -0,0 +1,15 @@
+# Guillaume Gillard
+# 11/12/2022
+
+class Node():
+    pass
+class LinkedList():
+    pass
+
+### Code to complete [START] ###
+
+def __init__(self, lst):
+    self.__length = len(lst)
+    self.__head = None 
+    for i in range(1, len(lst)+1):
+        self.__head = Node(lst[-i], self.__head)
