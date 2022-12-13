363d6dacd12d6a68d583efe9efb816c737500968
Guillaume <gillard.guillaume@gmail.com>
2022-12-13 10:15:24 +0100

    Create linkedlist-insert.py

---
+++ Session 11/linkedlist-insert.py
@@ -0,0 +1,28 @@
+# Guillaume Gillard 
+# 13/12/2022
+
+from linkedlist import LinkedList, Node 
+
+### Code to complete [START] ###
+
+def insert(self, s):
+    current = self.__head 
+    if current == None :
+        current = Node(s, current)
+        self.__head = current
+    elif s < current.value() :
+        current = Node(s, current)
+        self.__head = current
+    else :
+        while True :
+            if current.next() == None:
+                current.set_next(Node(s, current.next()))
+                break
+            elif s < current.next().value():
+                
+                current.set_next(Node(s, current.next()))
+                break   
+            current = current.next()
+    self.__length += 1
+    return
+        
