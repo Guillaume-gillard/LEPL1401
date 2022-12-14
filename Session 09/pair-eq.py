53898eef87af50c8e7d9bd60348b0cfbcf76a519
Guillaume <gillard.guillaume@gmail.com>
2022-12-14 19:41:25 +0100

    Update

---
+++ Session 09/pair-eq.py
@@ -0,0 +1,16 @@
+# Guillaume Gillard 
+# 14/12/2022
+
+
+### Code to complete [START] ###
+
+def __eq__(self, p):
+    """
+        @pre    -
+        @post   Retourne true si la paire p est égale à l'objet.
+                Retourne false sinon
+    """
+    if p == None :
+        return False
+    if self.a == p.a and self.b == p.b :
+        return True
