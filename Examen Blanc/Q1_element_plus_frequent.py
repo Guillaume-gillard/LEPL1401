2a607e827f051be86436dd9890098ecce825606f
Guillaume <gillard.guillaume@gmail.com>
2022-12-20 19:27:13 +0100

    Adding examen blanc

---
+++ Examen Blanc/Q1_element_plus_frequent.py
@@ -0,0 +1,29 @@
+# Guillaume Gillard
+# 20/12/2022
+
+
+def plus_frequent(l):
+    """
+    pre: `l` est une liste, len(l) > 0
+    post: retourne l'élément qui se trouve le plus grand nombre de fois
+        dans la liste `l` (nombre d'ocurrences égales au sens de ==).  Si
+        plusieurs éléments différents apparaissent un plus grand nombre égal
+        de fois, retourne le premier apparaissant dans la liste.
+    """
+
+    ### Code to complete [START] ###
+
+    count = 0
+    best = None
+    for item in l :
+        best_prev = count
+        count = l.count(item)
+        if best_prev < count :
+            best = item 
+    return best
+
+#### TESTS ####
+
+assert plus_frequent([1,2,3,1,2,2]) == 2, "Test 1 failled" # TEST 1
+assert plus_frequent([1,1,2,2,2,1,3,3,3,3,4,3]) == 3, "Test 2 failled" # TEST 2
+assert plus_frequent([1,2]) == 1, "Test 3 failled" # TEST 3
---
+++ Examen Blanc/Q2_probleme_precipitations.py
@@ -0,0 +1,31 @@
+# Guillaume Gillard
+# 20/12/2022
+
+def rainfall(l):
+    """
+    pre: l est une liste de nombres, len(l) > 0, l contient un élément 9999.
+    post: retourne la moyenne des éléments dans la liste l jusqu'à l'élément 9999
+        non compris.  Si des éléments sont négatifs, ils sont traités comme valant 0.
+    """
+
+    ### Code to complete [START] ###
+
+    mean = 0
+    count = 0
+    somme = 0
+    for item in l :
+        if item == 9999:
+            break 
+        if item > 0 :
+            somme += item 
+        count += 1
+    mean = somme/count 
+    return mean
+
+#### TESTS ####
+
+assert rainfall([100,50,50,250,200,9999]) == 130, "Test 1 failled" # TEST 1
+
+assert rainfall([2, 2]) == 2, "Test 2 failled" # TEST 2
+
+assert rainfall([-1, -3]) == 0, "Test 3 failled" # TEST 3
