99648230cb2665e8f529acfaf1337457c32d0d6e
Guillaume <gillard.guillaume@gmail.com>
2022-12-22 21:05:56 +0100

    Create 03_fusion_de_liste.py

---
+++ Session 05/03_fusion_de_liste.py
@@ -0,0 +1,14 @@
+# Guillaume Gillard 
+# 22/12/2022
+
+### Code to complete [START] ###
+
+def merge(first_list, second_list):
+    l = first_list.copy()
+    for x, y in second_list :
+        for i in range(len(l)):
+            if y <= l[i][1] :
+                l.insert(i, [x, y])
+                break
+    return l
+            
