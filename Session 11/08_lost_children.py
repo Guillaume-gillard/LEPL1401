6d0b3195c0cb85bc94fdbd0438eeaf0a9a5f79bf
Guillaume <gillard.guillaume@gmail.com>
2022-12-22 21:06:31 +0100

    Create 08_lost_children.py

---
+++ Session 11/08_lost_children.py
@@ -0,0 +1,20 @@
+# Guillaume Gillard
+# 22/12/2022
+
+class Child:
+    def is_next_valid():  #return a boolean
+        pass
+    def next_child():     #return a Child
+        pass
+
+    ### Code to complete [START] ###
+
+    def is_every_child_here(first_child):
+        current = first_child
+        while True :
+            if not current.is_next_valid():
+                return False
+            current = current.next_child()
+            if current == first_child :
+                return True
+
