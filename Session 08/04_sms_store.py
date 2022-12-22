f59abbcfdfe0969da6e0d470dfdb259379a64282
Guillaume <gillard.guillaume@gmail.com>
2022-12-22 21:07:25 +0100

    Create 04_sms_store.py

---
+++ Session 08/04_sms_store.py
@@ -0,0 +1,41 @@
+# Guillaume Gillard
+# 22/12/2022
+
+### Code to complete [START] ###
+
+class SMS_Store :
+    def __init__(self):
+        self.inbox = []
+
+    def add_new_arrival(self, from_number, time_arrived, text_of_SMS):
+        new_sms = (from_number, time_arrived, text_of_SMS)
+        self.inbox.append(new_sms)
+        self.has_been_viewed = False
+    
+    
+    def message_count(self):
+        number_of_message = len(self.inbox)
+        return number_of_message
+    
+
+    def get_unread_indexes(self):
+        unread_indexes = []
+        count = 0
+        for sms in self.inbox:
+            if not sms.has_been_viewed :
+                unread_indexes.append(count)
+            count += 1
+        return unread_indexes
+
+    def get_message(self, i):
+        sms_location = self.inbox[i]
+        sms = (sms_location[0], sms_location[1], sms_location[2])
+        return sms
+    
+    
+    def delete(self, i):
+        self.inbox.pop(i)
+    
+
+    def clear(self):
+        self.inbox = []
