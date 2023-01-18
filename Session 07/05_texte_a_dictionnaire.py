f68fbb2fa93112cf517610cca5559cdb50dca481
Guillaume <gillard.guillaume@gmail.com>
2023-01-18 09:36:45 +0100

    Create 05_texte_a_dictionnaire.py

---
+++ Session 07/05_texte_a_dictionnaire.py
@@ -0,0 +1,16 @@
+def create_dictionary(filename):
+    d = {}
+    with open(filename, "r") as file:
+        for line in file :
+            for word in line.split(' '):
+                if word in d :
+                    d[word] += 1
+                else :
+                    d[word] = 1
+        return d
+    
+def occ(dictionary, word):
+    try :
+        return dictionary[word]
+    except KeyError: 
+        return 0
