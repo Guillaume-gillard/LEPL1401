63a809be5d2cd7cb0016c5744c901d01756f33c5
Guillaume <gillard.guillaume@gmail.com>
2023-01-18 16:24:15 +0100

    Create 09_apocalypse_morse_translation.py

---
+++ Session 07/09_apocalypse_morse_translation.py
@@ -0,0 +1,20 @@
+morse = {
+"A" : ".-",
+"B" : "-...",
+"C" : "-.-.",
+"D" : "-..",
+"E" : ".",
+# ...
+}
+
+### Code to complete [START] ###
+
+def translate(data):
+    try :
+        traduction = ""
+        for x in data.strip():
+            traduction += morse[x.upper()]
+        return traduction
+    except :
+        raise ValueError("Unregistered character !")
+
