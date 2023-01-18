6a8b08054d2528a1e7b0d42413e20cc4e64466a3
Guillaume <gillard.guillaume@gmail.com>
2023-01-18 20:35:21 +0100

    Create calcul_note_finale.py

---
+++ calcul_note_finale.py
@@ -0,0 +1,7 @@
+def note_finale(interro, examen, bonus): # /20 /20 /1
+    if examen > interro :
+        note = examen + bonus
+    else : 
+        note = interro * (1/3) + examen (2/3) + bonus
+    return round(note)
+
